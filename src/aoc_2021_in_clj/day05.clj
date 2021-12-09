ecb83bed9f703889fcc10bf4e0a498b0ed3e6a80
Richard Barker <richbarker@gmail.com>
2021-12-09 22:02:06 +1100

    Day 5 part 2

--- src/aoc_2021_in_clj/day05.clj
+++ src/aoc_2021_in_clj/day05.clj
@@ -5,10 +5,8 @@
 (defn start-end [vent-line]
   (map #(mapv parse-int (-> %
                             (clojure.string/trim)
-                            (clojure.string/split #",")
-                            ))
+                            (clojure.string/split #",")))
        (clojure.string/split vent-line #"->")))
-
 
 (defn parse-vent [s]
   (let [[[x1 y1] [x2 y2]] (start-end s)]
@@ -21,8 +19,44 @@
       (for [i (range (min x1 x2) (inc (max x1 x2)))]
         [i y1]))))
 
+(defn op [a b] (cond (= a b) identity
+                     (< a b) inc
+                     (> a b) dec))
+
+(defn parse-vent-di [s]
+  (let [[[x1 y1] [x2 y2]] (start-end s)]
+
+    (loop [coords []
+           [x y] [x2 y2]]
+
+      (if (= [x y] [x1 y1])
+        (conj coords [x y])
+
+        (let [newx ((op x x1) x)
+              newy ((op y y1) y)]
+
+          (recur (conj coords [x y])
+                 [newx newy]))))))
+
 (defn part-1 [file-path]
   (let [coords (apply concat (map parse-vent (clojure.string/split-lines (slurp file-path))))
+        coord-counts
+               (reduce
+                 (fn [attr coord]
+                   (update attr coord (fnil inc 0)))
+                 {}
+                 coords)]
+
+    (count (filter
+             (fn [[k v]] (< 1 v))
+             coord-counts))
+    ))
+
+(defn part-2 [file-path]
+  (let [coords (apply concat
+                      (map parse-vent-di
+                           (clojure.string/split-lines
+                             (slurp file-path))))
         coord-counts
                (reduce
                  (fn [attr coord]
@@ -57,25 +91,25 @@
   (is (= 5774 (part-1 "resources/Day05.txt")))
   )
 
-(defn parse-numbers [number-data]
-  (clojure.string/split number-data #","))
+(deftest day05-part2-tests
 
-;(defn part-1 [file-path]
-;  (let [data (parse-file file-path)
-;        [bingo-i board] (winning-board-and-idx data)]
-;
-;    (calculate-overall-score (:drawn-numbers data) board bingo-i)))
-;
-;(defn part-2 [file-path]
-;  (let [data (parse-file file-path)
-;        [bingo-i board] (losing-board-and-idx data)]
-;
-;    (calculate-overall-score (:drawn-numbers data) board bingo-i)))
-;
-;(deftest overall-checks
-;
-;  (is (= 4512 (part-1 "resources/Day04_test.txt")) "test part 1")
-;  (is (= 50008 (part-1 "resources/Day04.txt")) "part 1")
-;
-;  (is (= 17408 (part-2 "resources/Day04.txt")) "part 2"))
-;
+  (is (= [[1 1] [2 2] [3 3]]
+         (sort (parse-vent-di "1,1 -> 3,3"))
+         ))
+
+  (is (= (sort [[1 1] [2 2] [3 3]])
+         (sort (parse-vent-di "3,3 -> 1,1"))
+         ))
+
+  (is (= (sort [[9 7], [8 8], [7 9]])
+         (sort (parse-vent-di "9,7 -> 7,9"))
+         ))
+
+  (is (= (sort [[9 7], [8 8], [7 9]])
+         (sort (parse-vent-di "7,9 -> 9,7"))
+         ))
+
+  (is (= 12 (part-2 "resources/Day05_test.txt")))
+  (is (= 18423 (part-2 "resources/Day05.txt")))
+  )
+
