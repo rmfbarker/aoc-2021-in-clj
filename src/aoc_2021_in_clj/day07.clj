78f70ebef5dcc02950593806af64dd856d83d9d4
Richard Barker <richbarker@gmail.com>
2021-12-10 20:07:04 +1100

    Day 7 complete

--- src/aoc_2021_in_clj/day07.clj
+++ src/aoc_2021_in_clj/day07.clj
@@ -4,21 +4,43 @@
 
 (defn parse-crabs [s] (map parse-int (clojure.string/split s #",")))
 
-(defn sum-of-dis [crabs pos]
+(defn simple-cost [a b] (Math/abs (- a b)))
+
+(defn sum-of-dis [cost-fn crabs pos]
   (apply + (map
-             (fn [crab] (Math/abs (- crab pos)))
+             (partial cost-fn pos)
              crabs)))
+
+(defn part-1 [crabs]
+  (let [crabs (sort crabs)]
+    (first (sort (map
+                   #(sum-of-dis simple-cost crabs %)
+                   (range (first crabs) (inc (last crabs))))))))
+
+(defn expensive-cost [a b]
+  (let [target (max a b)]
+    (loop [pos (min a b) cost 1 total 0]
+      (if (= target pos)
+        total
+        (recur (inc pos) (inc cost) (+ total cost))))))
+
+(defn part-2 [crabs]
+  (let [crabs (sort crabs)]
+    (first (sort (map
+                   #(sum-of-dis expensive-cost crabs %)
+                   (range (first crabs) (inc (last crabs))))))))
 
 (deftest test-day06
 
   (let [crabs (parse-crabs "16,1,2,0,4,2,7,1,2,14")]
-    (is (= 37 (sum-of-dis crabs 2)))
-    )
+    (is (= 37 (sum-of-dis simple-cost crabs 2)))
+    (is (= 37 (part-1 crabs)))
+    (is (= 37 (part-1 (parse-crabs (slurp "resources/Day07_test.txt")))))
+    (is (= 349769 (part-1 (parse-crabs (slurp "resources/Day07.txt")))))
 
-  ;(is (= 362666 (part-1 (clojure.string/trim (slurp "resources/Day06.txt"))
-  ;                      80)))
-  ;
-  ;(is (= 1640526601595 (part-1 (clojure.string/trim (slurp "resources/Day06.txt"))
-  ;                             256)))
-  )
+    (is (= 66 (expensive-cost 5 16)))
+    (is (= 66 (expensive-cost 16 5)))
+    (is (= 45 (expensive-cost 14 5)))
+    (is (= 168 (part-2 crabs)))
+    (is (= 99540554 (part-2 (parse-crabs (slurp "resources/Day07.txt")))))))
 
